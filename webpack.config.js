f4813d083c10da0cfa857a66ed06aa3fd5e43138
Paul George <paul.george@outlook.in>
2021-07-28 23:02:18 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/app.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 3300,
+    contentBase: path.resolve(__dirname, 'src'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg|svg|bmp)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Paul George Pullormadam',
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+  ],
+};
